4a79efd3a6fe752cbc982c0c66809dfb4a22aa23
umanskky <umanskky@gmail.com>
2023-10-17 11:53:24 +0300

    Изменил название окна

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ static void on_activate (GtkApplication *app) {
   // Create a new window
   GtkWidget *window = gtk_application_window_new (app);
   // Create a new button
-  GtkWidget *button = gtk_button_new_with_label ("Hello, World Саша Уманский!");
+  GtkWidget *button = gtk_button_new_with_label ("Hello, World Уманский!"); // Название окна
   // When the button is clicked, close the window passed as an argument
   g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_close), window);
   gtk_window_set_child (GTK_WINDOW (window), button);
